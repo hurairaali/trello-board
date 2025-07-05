f2f9096ff3c79b1e553a78c3a32af1723f0c932b
hurairaali <huraira@alphabase.co>
2025-07-05 19:38:43 +0500

    feat(ui): style card list

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 // components/Column.tsx
 import { FC } from "react";
+import { Plus, MoreHorizontal } from "lucide-react";
 
 interface ColumnProps {
   title: string;
@@ -7,9 +8,18 @@ interface ColumnProps {
 
 const Column: FC<ColumnProps> = ({ title }) => {
   return (
-    <div className="bg-white w-64 rounded shadow-md p-4 flex-shrink-0">
-      <h3 className="font-semibold text-lg mb-2">{title}</h3>
-      {/* Cards would go here in the future */}
+    <div className="bg-[#181818] rounded-lg shadow-md p-4 flex flex-col justify-between h-fit w-72">
+      <div className="flex justify-between items-start mb-4">
+        <span className="ds-text  lowercase text-md">{title}</span>
+        <MoreHorizontal size={20} className="text-white" />
+      </div>
+
+      <div className="mt-auto">
+        <button className="flex items-center text-gray-400 text-sm hover:underline">
+          <Plus size={14} className="mr-1" />
+          Add a card
+        </button>
+      </div>
     </div>
   );
 };
