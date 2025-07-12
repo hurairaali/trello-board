1e44c5b9e76e3f071247afe25c271965de0c2fb4
hurairaali <huraira@alphabase.co>
2025-07-12 16:01:35 +0500

    fix: Chnage text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[var(--font-geist-sans)]">
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-4xl font-bold">Welcome to My Trello Board</h1>
-        <p className="text-lg">
+        <h1 className="text-4xl font-bold text-white">
+          Welcome to My Trello Board
+        </h1>
+        <p className="text-lg text-white">
           This is a simple Trello board clone built with Next.js and Zustand.
         </p>
         <div className="pt-8">
